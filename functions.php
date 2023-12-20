ffc896dcd1bd37ecc48b5ab00e7eac6ec0a8b10e
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-12-20 05:32:48 +0100

    Generazione file  functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+//ricevere come argumenti gli array e la lungheza che inserira l'utente
+function generaPassword($arg1, $arg2, $arg3, $lunghezza){
+    //Unisci gli array di caratteri in un unico array
+    $caratteriPossibili = array_merge($arg1, $arg2, $arg3);
+    $password = " "; // Inizializza la password come stringa vuota
+
+
+    // Ciclo per generare la password con l'argumento lungheza che da l'utente
+    for ($i = 0; $i < $lunghezza; $i++){
+        //Selezona casualmente un carattere dall'array di caratteri
+        $carattereCasuale = $caratteriPossibili[array_rand($caratteriPossibili)];
+
+        // Aggiungi il carattere alla password contattenandola alla stringa vuota
+        $password .= $carattereCasuale;
+    }
+
+    return $password;
+
+}
--- password.php
+++ password.php
@@ -1,4 +1,6 @@
 <?php
+
+include  __DIR__ . "/functions.php";
 // Array per ottenere la password
 $lettereMaiuscoleMinuscole = str_split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 $numeri = str_split('0123456789');
@@ -15,24 +17,6 @@ if(isset($_GET["lunghezza"])){
     echo "Inserisce la lunghezza della password.";
 }
 
-//ricevere come argumenti gli array e la lungheza che inserira l'utente
-function generaPassword($arg1, $arg2, $arg3, $lunghezza){
-    //Unisci gli array di caratteri in un unico array
-    $caratteriPossibili = array_merge($arg1, $arg2, $arg3);
-    $password = " "; // Inizializza la password come stringa vuota
 
-
-    // Ciclo per generare la password con l'argumento lungheza che da l'utente
-    for ($i = 0; $i < $lunghezza; $i++){
-        //Selezona casualmente un carattere dall'array di caratteri
-        $carattereCasuale = $caratteriPossibili[array_rand($caratteriPossibili)];
-
-        // Aggiungi il carattere alla password contattenandola alla stringa vuota
-        $password .= $carattereCasuale;
-    }
-
-    return $password;
-
-}
 ?>
 
